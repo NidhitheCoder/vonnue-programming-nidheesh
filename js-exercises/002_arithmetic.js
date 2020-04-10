3c3c606bff3ae0685d3e3a56ae06825ea050b8d4
NidhitheCoder <nidhpv@gmail.com>
2020-04-10 23:40:53 +0530

    fix bugs

--- js-exercises/002_arithmetic.js
+++ js-exercises/002_arithmetic.js
@@ -1,9 +1,9 @@
 // npm install readline-sync  --> for getting iput from terminal
 
-var readline = require("readline-sync");
+const readline = require("readline-sync");
 
 function add(num1, num2) {
-  return num1 + num2;
+  return parseInt(num1) + parseInt(num2);
 }
 
 function substract(num1, num2) {
@@ -34,22 +34,22 @@ function isEqual(num1, num2) {
   return num1 === num2;
 }
 
-var number1 = readline.question("enter first Number :");
-var number2 = readline.question("Enter second Number :");
+let number1 = readline.question("enter first Number :");
+let number2 = readline.question("Enter second Number :");
 
 console.log("added");
 console.log(add(number1, number2));
-console.log("\n divide :");
+console.log("\ndivide :");
 console.log(divide(number1, number2));
-console.log("\n multiplication :");
+console.log("\nmultiplication :");
 console.log(multiply(number1, number2));
-console.log("\n substract :");
+console.log("\nsubstract :");
 console.log(substract(number1, number2));
-console.log("\n modulas :");
+console.log("\nmodulas :");
 console.log(modulas(number1, number2));
-console.log("\n check first number is graterthan second number :");
+console.log("\ncheck first number is graterthan second number :");
 console.log(greaterthan(number1, number2));
-console.log("\n check first number is lessthan second number :");
+console.log("\ncheck first number is lessthan second number :");
 console.log(lessthan(number1, number2));
-console.log("\n check first number is equal to second number :");
+console.log("\ncheck first number is equal to second number :");
 console.log(isEqual(number1, number2));
