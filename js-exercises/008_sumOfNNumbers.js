220fed43028c25112d79e23488b5eef311e86525
NidhitheCoder <nidhpv@gmail.com>
2020-04-11 02:30:59 +0530

    Find Sum of N numbers

--- js-exercises/008_sumOfNNumbers.js
+++ js-exercises/008_sumOfNNumbers.js
@@ -1,9 +1,9 @@
 const readline = require("readline-sync");
 
 let num = readline.question("Enter a number :");
-sum=0;
+sum = 0;
 
-for (i=1;i<=num;i++){
-    sum+=i;
+for (i = 1; i <= num; i++) {
+  sum += i;
 }
-console.log(sum)
+console.log(sum);
