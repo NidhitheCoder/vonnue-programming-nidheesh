6a20d223c75de7997f8aa662915e9216f3c1af24
NidhitheCoder <nidhpv@gmail.com>
2020-04-14 23:54:14 +0530

    use es6 format

--- js-exercises/008_sumOfNNumbers.js
+++ js-exercises/008_sumOfNNumbers.js
@@ -3,7 +3,7 @@ const readline = require("readline-sync");
 let number = readline.question("Enter a number :");
 sum = 0;
 const findSum = (sum, num) => {
-  for (i = 1; i <= num; i++) {
+  for (let i = 1; i <= num; i++) {""
     sum += i;
   }
   return sum;
