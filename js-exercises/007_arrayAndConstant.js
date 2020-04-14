d8d561da2e9e43a9ee62ba880604b1ddbaadbd69
NidhitheCoder <nidhpv@gmail.com>
2020-04-14 23:38:52 +0530

    use function

--- js-exercises/007_arrayAndConstant.js
+++ js-exercises/007_arrayAndConstant.js
@@ -5,8 +5,10 @@ let result = [];
 arr = readline.question("Enter array decimal elements separated by comma:");
 
 let constant = readline.question("Enter constant value :");
-
-result = arr.split(",").map(el => {
-  return constant * parseInt(el);
-});
-console.log(result);
+const addConstant = (arr, constant) => {
+  result = arr.split(",").map(el => {
+    return constant * parseInt(el);
+  });
+  return result;
+};
+console.log(addConstant(arr, constant));
