9f0b5fe3d08ce1ab366bf52cfaa63c8c56917a2c
NidhitheCoder <nidhpv@gmail.com>
2020-04-16 00:30:46 +0530

    loop variable declared

--- js-exercises/015_isPowerOf3.js
+++ js-exercises/015_isPowerOf3.js
@@ -2,7 +2,7 @@ const readline = require("readline-sync");
 let number = readline.question("enter a number :");
 const check = num => {
   let result = 1;
-  for (i = 1; i <= num; i++) {
+  for (let i = 1; i <= num; i++) {
     result *= 3;
     if (result == num) {
       return "This number is power of 3 of " + i;
