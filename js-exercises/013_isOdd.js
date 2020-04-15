6ce7a5612936a2bf9d47757a296b9c54c614e4c2
NidhitheCoder <nidhpv@gmail.com>
2020-04-16 00:15:36 +0530

    find out odd or even

---
+++ js-exercises/013_isOdd.js
@@ -0,0 +1,10 @@
+const readline = require("readline-sync");
+let number = readline.question("enter a positive number :");
+const checkOdd = (num) => {
+  if (num % 2 != 0) {
+    return "this is an odd number";
+  } else {
+    return "this this is an even number";
+  }
+};
+console.log(checkOdd(number));
