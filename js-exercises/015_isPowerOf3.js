6fddb9c1c7a7f7ae8f4d933cb9daa43cee975967
NidhitheCoder <nidhpv@gmail.com>
2020-04-17 00:32:38 +0530

    remove  const from function declaration

--- js-exercises/015_isPowerOf3.js
+++ js-exercises/015_isPowerOf3.js
@@ -1,14 +1,18 @@
 const readline = require("readline-sync");
+
 let number = readline.question("enter a number :");
-const check = num => {
+
+checkPower = num => {
   let result = 1;
+  
   for (let i = 1; i <= num; i++) {
     result *= 3;
-    if (result == num) {
+    if (result === num) {
       return "This number is power of 3 of " + i;
     }
   }
 
   return "This number is not a power of 3 of any number";
 };
-console.log(check(number));
+
+console.log(checkPower(number));
