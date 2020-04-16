da291da89b9034d234f1a633a1b55dc2b2025c25
NidhitheCoder <nidhpv@gmail.com>
2020-04-17 00:58:45 +0530

    change console result to return array

--- js-exercises/018_Nxt20LeapYear.js
+++ js-exercises/018_Nxt20LeapYear.js
@@ -1,19 +1,25 @@
-const twentytimes = yr => {
+twentytimes = yr => {
   let nextYear = yr;
-  for (let i = 0; i < 20; i++) {
-    console.log(nextYear);
+  let arr = [];
+  for (let j = 0; j < 20; j++) {
+    arr.push(nextYear);
     nextYear = nextYear + 4;
+  }
+  return arr;
+};
+
+printLeapYears = () => {
+  if (year % 4 === 0) {
+    return twentytimes(year);
+  } else {
+    for (let i = 1; i <= 3; i++) {
+      year = year + 1;
+      if (year % 4 === 0) {
+        return twentytimes(year);
+      }
+    }
   }
 };
 
 let year = new Date().getFullYear();
-if (year % 4 == 0) {
-  twentytimes(year);
-} else {
-  for (let i = 1; i <= 3; i++) {
-    year = year + 1;
-    if (year % 4 == 0) {
-      twentytimes(year);
-    }
-  }
-}
+console.log(printLeapYears(year));
