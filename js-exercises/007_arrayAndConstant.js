b81a6777463ee7a5e5b928e9b6e7dd94df16cf4c
NidhitheCoder <nidhpv@gmail.com>
2020-04-16 23:46:22 +0530

    change const from function declaration

--- js-exercises/007_arrayAndConstant.js
+++ js-exercises/007_arrayAndConstant.js
@@ -1,11 +1,12 @@
 const readline = require("readline-sync");
 
-let arr = [];
-let result = [];
-arr = readline.question("Enter array decimal elements separated by comma:");
+let arr = [],
+  result = [];
 
+arr = readline.question("Enter array decimal elements separated by comma:");
 let constant = readline.question("Enter constant value :");
-const multiplyWtConstant = (arr, constant) => {
+
+multiplyWtConstant = (arr, constant) => {
   result = arr.split(",").map(el => {
     return constant * parseInt(el);
   });
