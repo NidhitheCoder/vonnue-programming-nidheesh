f39a3c679db10838597c0113933c113699ab1a23
NidhitheCoder <nidhpv@gmail.com>
2020-04-17 01:17:04 +0530

    change const from declaring function

--- js-exercises/022_stringType.js
+++ js-exercises/022_stringType.js
@@ -1,16 +1,19 @@
 const readline = require("readline-sync");
+
 let value = readline.question("Enter a value :");
-const checktype = str => {
+
+checkType = str => {
   var format = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
 
   if (str.match(format)) {
     return "this is  special character";
-  } else if (str == str.toLowerCase()) {
+  } else if (str === str.toLowerCase()) {
     return "this string is lowercase";
-  } else if (str == str.toUpperCase()) {
+  } else if (str === str.toUpperCase()) {
     return "this string is uppercase";
   } else {
     return "other type";
   }
 };
-console.log(checktype(value));
+
+console.log(checkType(value));
