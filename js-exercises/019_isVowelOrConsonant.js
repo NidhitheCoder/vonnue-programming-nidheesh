4babd327de6c7e92348a90a537b7984cfb5423a4
NidhitheCoder <nidhpv@gmail.com>
2020-04-17 01:03:58 +0530

    remove const from function declaration

--- js-exercises/019_isVowelOrConsonant.js
+++ js-exercises/019_isVowelOrConsonant.js
@@ -2,12 +2,14 @@ const readline = require("readline-sync");
 
 let letter = readline.question("please enter a letter :");
 let vowels = ["a", "e", "i", "o", "u"];
-const checkVowel = letter => {
+
+checkVowel = letter => {
   for (let i = 0; i <= 5; i++) {
-    if (vowels[i] == letter) {
+    if (vowels[i] === letter) {
       return "this is a vowel";
     }
   }
   return "this is consonant";
 };
+
 console.log(checkVowel(letter));
