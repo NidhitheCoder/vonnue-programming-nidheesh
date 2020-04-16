bae7c5da05da8d3086515423809f1c6465cfda57
NidhitheCoder <nidhpv@gmail.com>
2020-04-17 01:56:56 +0530

    optimize code

--- js-exercises/016_printPrimeNumber.js
+++ js-exercises/016_printPrimeNumber.js
@@ -1,17 +1,24 @@
 const readline = require("readline-sync");
+
 let number = readline.question("enter a number :");
-const printPrime = num => {
+
+printPrime = num => {
+  let result = [];
   for (let i = 2; i <= num; i++) {
-    let flag = true;
-    let mid = Math.floor(i / 2) + 1;
-    for (let j = 2; j <= mid; j++) {
-      if (i % j == 0 && i != 2) {
-        flag = false;
+    let flag = 0;
+    let mid = Math.floor(i / 2);
+
+    for (let j = 1; j <= mid; j++) {
+      if (i % j === 0) {
+        flag += 1;
       }
     }
-    if (flag == true) {
-      console.log(i);
+
+    if (flag === 1) {
+      result.push(i);
     }
   }
+  return result;
 };
-printPrime(number);
+
+console.log(printPrime(number));
