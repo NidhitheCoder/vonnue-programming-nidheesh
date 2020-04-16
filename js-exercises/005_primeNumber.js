906cb8230c134d503e013ec3bfbbabf39360637c
NidhitheCoder <nidhpv@gmail.com>
2020-04-16 11:54:49 +0530

    change == to ===

--- js-exercises/005_primeNumber.js
+++ js-exercises/005_primeNumber.js
@@ -1,19 +1,18 @@
 const readline = require("readline-sync");
 let num = readline.question("enter a positive number :");
 const checkPrime = val => {
-  if (val == 1) {
+  if (val === 1) {
     return "it is not a prime number";
   }
   let flag = true;
-  let mid=Math.floor(val/2)+1;
-  for (let i =mid ; i > 1; i--) {
-
-    if (val % i == 0) {
+  let mid = Math.floor(val / 2) + 1;
+  for (let i = mid; i > 1; i--) {
+    if (val % i === 0) {
       flag = false;
     }
   }
 
-  if (flag == true) {
+  if (flag === true) {
     return "it is a prime number";
   }
   return "it is not a prime number";
