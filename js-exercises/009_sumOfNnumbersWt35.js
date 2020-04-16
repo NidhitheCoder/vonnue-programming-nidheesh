69d23de9928a1f56398536b479d7d75762f2b09a
NidhitheCoder <nidhpv@gmail.com>
2020-04-16 23:50:32 +0530

    change const from function declaration

--- js-exercises/009_sumOfNnumbersWt35.js
+++ js-exercises/009_sumOfNnumbersWt35.js
@@ -2,13 +2,14 @@ const readline = require("readline-sync");
 
 let number = readline.question("Enter a number :");
 
-const sumWt3And5 = num => {
+sumWt3And5 = num => {
   let sum = 0;
   for (let i = 1; i <= num; i++) {
-    if (i % 3 == 0 || i % 5 == 0) {
+    if (i % 3 === 0 || i % 5 === 0) {
       sum += i;
     }
   }
   return sum;
 };
+
 console.log(sumWt3And5(number));
