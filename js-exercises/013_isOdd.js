b6b5aac5af698fea1aff4226c192e3066a5cccae
NidhitheCoder <nidhpv@gmail.com>
2020-04-17 00:17:54 +0530

    avoid const at function declaration

--- js-exercises/013_isOdd.js
+++ js-exercises/013_isOdd.js
@@ -1,10 +1,13 @@
 const readline = require("readline-sync");
+
 let number = readline.question("enter a positive number :");
-const checkOdd = (num) => {
-  if (num % 2 != 0) {
+
+checkOdd = num => {
+  if (num % 2 !== 0) {
     return "this is an odd number";
   } else {
     return "this this is an even number";
   }
 };
+
 console.log(checkOdd(number));
