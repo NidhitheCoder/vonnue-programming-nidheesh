77bed8a129d2db769d694b5a6442b456eb4f167f
NidhitheCoder <nidhpv@gmail.com>
2020-04-17 00:12:55 +0530

    avoid const at function declaration

--- js-exercises/012_primeNumber.js
+++ js-exercises/012_primeNumber.js
@@ -1,21 +1,24 @@
 const readline = require("readline-sync");
+
 let num = readline.question("enter a positive number :");
-const checkPrime = val => {
-  if (val == 1) {
+
+checkPrimeNumber = val => {
+  if (val === 1) {
     return "it is not a prime number";
   }
   let flag = true;
   let mid = Math.floor(val / 2) + 1;
+
   for (let i = mid; i > 1; i--) {
-    if (val % i == 0) {
+    if (val % i === 0) {
       flag = false;
     }
   }
 
-  if (flag == true) {
+  if (flag === true) {
     return "it is a prime number";
   }
   return "it is not a prime number";
 };
 
-console.log(checkPrime(num));
+console.log(checkPrimeNumber(num));
