d85006250bd3dc48213a82cbe661cc4f5e553266
NidhitheCoder <nidhpv@gmail.com>
2020-04-16 23:25:44 +0530

    bug fix

--- js-exercises/005_primeNumber.js
+++ js-exercises/005_primeNumber.js
@@ -1,11 +1,14 @@
 const readline = require("readline-sync");
+
 let num = readline.question("enter a positive number :");
-const checkPrime = val => {
+
+const checkPrimeNUmber = val => {
   if (val === 1) {
     return "it is not a prime number";
   }
   let flag = true;
   let mid = Math.floor(val / 2) + 1;
+
   for (let i = mid; i > 1; i--) {
     if (val % i === 0) {
       flag = false;
@@ -15,7 +18,8 @@ const checkPrime = val => {
   if (flag === true) {
     return "it is a prime number";
   }
+
   return "it is not a prime number";
 };
 
-console.log(checkPrime(num));
+console.log(checkPrimeNUmber(num));
