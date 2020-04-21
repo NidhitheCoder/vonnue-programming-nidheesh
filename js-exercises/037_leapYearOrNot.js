fa965cd09940062fba9cd5f4bef1723532fe7038
NidhitheCoder <nidhpv@gmail.com>
2020-04-22 00:43:26 +0530

    changes

--- js-exercises/037_leapYearOrNot.js
+++ js-exercises/037_leapYearOrNot.js
@@ -3,7 +3,7 @@ const readline = require("readline-sync");
 let year = parseInt(readline.question("enter the year :"));
 
 checkLeapYear = yr => {
-  if (yr % 4 == 0) {
+  if (yr % 4 === 0) {
     return "this is a leap year";
   }
   return "this is not a leap year";
