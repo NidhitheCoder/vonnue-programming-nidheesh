110503cb62e511bad0e48fc34a87ed4f2dfc7c56
NidhitheCoder <nidhpv@gmail.com>
2020-04-22 00:38:19 +0530

    changes

--- js-exercises/033_sumOfNumbersByRange.js
+++ js-exercises/033_sumOfNumbersByRange.js
@@ -3,7 +3,7 @@ const readline = require("readline-sync");
 let start = parseInt(readline.question("enter the starting point:"));
 let stop = parseInt(readline.question("enter the ending point :"));
 
-const findSumByRange = (start, stop) => {
+findSumByRange = (start, stop) => {
   let sum = 0;
   for (let i = start; i <= stop; i++) {
     sum += i;
