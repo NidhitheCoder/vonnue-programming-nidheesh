180f2622389fa56c706e52cd2d8fc91c3c2d94f3
NidhitheCoder <nidhpv@gmail.com>
2020-04-22 00:16:42 +0530

    findout factorial of a number

---
+++ js-exercises/035_factorialOfANumber.js
@@ -0,0 +1,13 @@
+const readline = require("readline-sync");
+
+let number = readline.question("Enter a number :");
+
+const factorial = num => {
+  let product = 1;
+  for (i = 1; i <= num; i++) {
+    product *= i;
+  }
+  return product;
+};
+
+console.log(factorial(number));
