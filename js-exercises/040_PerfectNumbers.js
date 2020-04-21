fca9715ccb323703b4e6bdcf39f4ce293b2c131c
NidhitheCoder <nidhpv@gmail.com>
2020-04-22 01:19:30 +0530

    check perfect number or not

---
+++ js-exercises/040_PerfectNumbers.js
@@ -0,0 +1,21 @@
+const readline = require("readline-sync");
+
+let num = parseInt(readline.question("enter a number here :"));
+
+checkPerfectNumber = number => {
+  let midValue = Math.floor(number / 2);
+  let divSum = 0;
+
+  for (let i = 1; i <= midValue; i++) {
+    if (number % i === 0) {
+      divSum += i;
+    }
+  }
+  
+  if (number === divSum) {
+    return "this is a perfect number";
+  }
+  return "this is not a perfect number";
+};
+
+console.log(checkPerfectNumber(num));
