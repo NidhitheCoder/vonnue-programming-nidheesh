4cb6c22fc09e3e7579dca3d9e732bf5a82dfaca8
NidhitheCoder <nidhpv@gmail.com>
2020-04-22 21:39:35 +0530

    reduce code size

--- js-exercises/027_gcdOfTwoNumbers.js
+++ js-exercises/027_gcdOfTwoNumbers.js
@@ -1,29 +1,10 @@
-const readline = require("readline-sync");
-
-let number1 = parseInt(readline.question("enter the first number : "));
-let number2 = parseInt(readline.question("enter the second number : "));
-
-findFactors = num => {
-  let arrFactors = [num];
-  for (let i = Math.floor(num / 2); i > 0; i--) {
-    if (num % i === 0) {
-      arrFactors.push(i);
-    }
-  }
-  return arrFactors;
-};
-
 findGcd = (num1, num2) => {
-  let num1Fact, num2Fact;
-  num1Fact = findFactors(num1);
-  num2Fact = findFactors(num2);
-
-  for (let j = 0; j <= num1Fact.length - 1; j++) {
-    for (let k = 0; k <= num2Fact.length - 1; k++) {
-      if (num1Fact[j] === num2Fact[k]) {
-        return num1Fact[j];
-      }
+  let min = Math.min(num1, num2);
+  for (let i = min; i > 0; i--) {
+    if (num1 % i === 0 && num2 % i === 0) {
+      return i;
     }
   }
 };
-console.log(findGcd(number1, number2));
+
+console.log("The Greatest Common divisor is ", findGcd(10, 5));
