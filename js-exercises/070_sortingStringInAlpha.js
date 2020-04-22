ce403c4681813e280779615cff601cf3e2be2e34
NidhitheCoder <nidhpv@gmail.com>
2020-04-22 16:59:20 +0530

    remove const

--- js-exercises/070_sortingStringInAlpha.js
+++ js-exercises/070_sortingStringInAlpha.js
@@ -1,16 +1,17 @@
 let stringValue = "Qwerty";
 
-const sortString = str => {
+sortString = str => {
   let arr = str.split("");
 
   for (let i = 0; i <= arr.length - 1; i++) {
     for (let i = 0; i < arr.length; i++) {
-      
+
       if (arr[i] > arr[i + 1]) {
         let temp = arr[i + 1];
         arr[i + 1] = arr[i];
         arr[i] = temp;
       }
+      
     }
   }
 
