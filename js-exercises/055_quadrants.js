5c81af24b528340e2817e87a8d143e484736a66a
NidhitheCoder <nidhpv@gmail.com>
2020-04-22 22:09:20 +0530

    Quadrants in which coordinates lie

---
+++ js-exercises/055_quadrants.js
@@ -0,0 +1,13 @@
+findQuadrant = (x, y) => {
+  if (x > 0 && y > 0) return "lies in First quadrant";
+  else if (x < 0 && y > 0) return "lies in Second quadrant";
+  else if (x < 0 && y < 0) return "lies in Third quadrant";
+  else if (x > 0 && y < 0) return "lies in Fourth quadrant";
+  else if (x === 0 && y > 0) return "lies at positive y axis";
+  else if (x === 0 && y < 0) return "lies at negative y axis";
+  else if (y === 0 && x < 0) return "lies at negative x axis";
+  else if (y === 0 && x > 0) return "lies at positive x axis";
+  else return "lies at origin";
+};
+
+console.log(findQuadrant(4, -3));
