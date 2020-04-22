963690306ee6ab638eaf0c06f1dff3527c7694b7
NidhitheCoder <nidhpv@gmail.com>
2020-04-22 16:43:28 +0530

    changes

--- js-exercises/068_countVowels.js
+++ js-exercises/068_countVowels.js
@@ -4,7 +4,6 @@ let stringValue = "javascript";
 const countVowel = str => {
   let vowels = ["a", "e", "i", "o", "u"];
   let count = 0;
-
   for (i = 0; i < str.length; i++) {
     vowels.forEach(val => {
       if (val === str[i]) {
