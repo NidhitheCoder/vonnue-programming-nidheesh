7fc3457dcd216e7e272bf200024a847f241c4329
NidhitheCoder <nidhpv@gmail.com>
2020-04-22 16:22:19 +0530

    changes

--- js-exercises/065_checkAnElement.js
+++ js-exercises/065_checkAnElement.js
@@ -2,7 +2,6 @@ let array = [4, 3, 55, 4, 6, 33, 5, 22, 99, 33];
 let number = 22;
 
 checkElement = (arr, num) => {
-  
   let result = arr.filter(function(el) {
     return el === num ? true : false;
   });
