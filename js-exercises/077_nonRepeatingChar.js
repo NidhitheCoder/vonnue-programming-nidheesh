50be591828b53815b032f06c19dd500c501fadb3
NidhitheCoder <nidhpv@gmail.com>
2020-05-07 01:05:14 +0530

    for good readability

--- js-exercises/077_nonRepeatingChar.js
+++ js-exercises/077_nonRepeatingChar.js
@@ -1,21 +1,25 @@
 const readline = require("readline-sync");
 let string = readline.question("enter the string :");
 
-findCount = str => {
+findCount = (arr,i) => {
+  let count = 0;
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[i] === arr[j]) {
+      count++;
+    }
+  }
+  return count;
+};
+
+findNonRpeatingCharacters = str => {
   let arr = str.split(""),
     stringArr = [];
   for (let i = 0; i < arr.length; i++) {
-    let count = 0;
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[i] === arr[j]) {
-        count++;
-      }
-    }
-    if (count === 1) {
+    if (findCount(arr,i) === 1) {
       stringArr.push(arr[i]);
     }
   }
   return stringArr;
 };
 
-console.log(findCount(string));
+console.log(findNonRpeatingCharacters(string));
