11b8636247f56924d61dbff99f9d6505d561cd54
NidhitheCoder <nidhpv@gmail.com>
2020-05-12 03:13:23 +0530

    add array sorting too.

--- js-exercises/097_tripletes.js
+++ js-exercises/097_tripletes.js
@@ -1,7 +1,11 @@
-let arr = [1, 5, 6, 11, 17];
+let arr = [1, 11, 6, 17];
 
-countTripletes = (arr) => {
+countTripletes = arr => {
   let count = 0;
+  arr.sort((a, b) => {
+    return a - b;
+  });
+  
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] + arr[i + 1] === arr[i + 2]) {
       count++;
