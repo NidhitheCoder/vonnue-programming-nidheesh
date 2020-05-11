5e21344e3a943a7df9423b62ddae891cd8ae9695
NidhitheCoder <nidhpv@gmail.com>
2020-05-11 11:59:45 +0530

    print elements on odd positions

---
+++ js-exercises/094_oddPositionElements.js
@@ -0,0 +1,12 @@
+const readline = require("readline-sync");
+let arr = [2, 1, 3, 5, 4, 6, 8, 6, 9, 8],
+  result = [];
+
+findOddPositions = arr => {
+  for (let i = 1; i < arr.length; i = i + 2) {
+    result.push(arr[i]);
+  }
+  return result;
+};
+
+console.log(findOddPositions(arr));
